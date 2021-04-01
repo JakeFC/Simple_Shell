834cd9ae6327614c0c1c11bc231dc0d5c3659b32
HLozano12 <HLR8503@gmail.com>
2021-04-01 19:40:57 +0000

    Header

---
+++ tom_shelleck.h
@@ -0,0 +1,17 @@
+#ifndef TOM_SHELLECK_H
+#define TOM_SHELLECK_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/type.h>
+#include <sys/wait.h>
+
+
+
+/* prototypes */
+
+
+#endif /* TOM_SHELLECK_H */
