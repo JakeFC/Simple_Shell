b427f439f93c7d3340c7a251510bbe357125206d
HLozano12 <HLR8503@gmail.com>
2021-04-09 20:07:30 +0000

    Betty & Noted

--- cleanup.c
+++ cleanup.c
@@ -1,12 +1,11 @@
 #include "tom_shelleck.h"
-
 /**
  * string_switch - frees a string and replaces it with another
  * @prev: address of string to replace
- * @new: address of string to place into prev address
+ * @tmp: address of string to place into prev address
  * Return: 0 on success, 1 on failure
  */
-int string_switch(char **prev,char **tmp)
+int string_switch(char **prev, char **tmp)
 {
 	free(*prev);
 	*prev = _strdup(*tmp);
